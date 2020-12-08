4dbc65a13153b0e62f5518f3ea33db506ae5a3ed
Raviraj Parab <ravirajparab44@gmail.com>
2020-12-08 15:36:46 +0530

    dates logic corrected

--- utilities.js
+++ utilities.js
@@ -12,87 +12,111 @@ const getExchangeData =()=>{
 }
 
 const getPLForSymbol =async (i)=>{
-    const sid=i.sid;
-    if(sid){
-        const intraDataUrl =`https://api.tickertape.in/stocks/charts/intra/${sid}`;
-        const sidProm = await fetch(intraDataUrl);
-        const sidData = await sidProm.json();
-        let priceTimeData = sidData.data[0].points.slice(53);
-        priceTimeData = priceTimeData.splice(0,priceTimeData.length-53);
-        
-        //find the shorting Price
-        const shp= (priceTimeData.filter(i=>i.ts.split('T')[1]==="04:40:00.000Z"))[0].lp;
-        const sl= shp+slfactor*shp;
-        const target = shp-targetfactor*shp;
-        const qty = Math.round(inv/shp);
-        const pl=0;
-        for (let index = 0; index < priceTimeData.length; index++) {
-            const element = priceTimeData[index];
-            if(element.lp>=sl){
-                const timeSLHitLocal = new Date(element.ts).toLocaleTimeString();
-               
-                return({
-                    Symbol :i.Symbol,
-                    Type :'SL Hit',
-                    SP : shp,
-                    Price : element.lp,
-                    PL : Math.round(qty*(shp-sl)-taxFactor*2*inv),
-                    Timing : timeSLHitLocal,
-                    Qty :qty
-                })  ;         
-               // break;
-            }else if(element.lp<=target){
-                const timeTargetHitLocal = new Date(element.ts).toLocaleTimeString();
-               
-                return({
-                    Symbol :i.Symbol,
-                    Type :'Target Hit',
-                    SP : shp,
-                    Price : element.lp,
-                    PL : Math.round(qty*(shp-target)-taxFactor*2*inv),
-                    Timing : timeTargetHitLocal,
-                    Qty :qty
-                })  ;    
-               // break;
-            }else{
-                if(index===priceTimeData.length-1){
-                    const timeLocal = new Date(element.ts).toLocaleTimeString();
-
+    try{
+        const sid=i.sid;
+        if(sid){
+            const intraDataUrl =`https://api.tickertape.in/stocks/charts/intra/${sid}?dt=${new Date().toString()}`;
+            const sidProm = await fetch(intraDataUrl);
+            const sidData = await sidProm.json();
+            //new logic
+            const fi=sidData.data[0].points.findIndex(i=>i.ts.split('T')[1]==="04:40:00.000Z");
+            const fid=sidData.data[0].points.slice(fi);
+            const si = fid.findIndex(i=>i.ts.split('T')[1]==="09:00:00.000Z");
+            if(si>=-1){
+                fid.splice(si-fid.length+1);
+               // console.log(fid[0]);
+               // console.log(fid[fid.length-1]);
+            }
+            
+            
+            //find the shorting Price
+            const shp= fid[0].lp;
+            const sl= shp+slfactor*shp;
+            const target = shp-targetfactor*shp;
+            const qty = Math.round(inv/shp);
+            const pl=0;
+            for (let index = 0; index < fid.length; index++) {
+                const element = fid[index];
+                
+                if(element.lp>=sl){
+                    const timeSLHitLocal = new Date(element.ts).toLocaleTimeString();
+                   
                     return({
                         Symbol :i.Symbol,
-                        Type :'Closing',
+                        Type :'SL Hit',
                         SP : shp,
                         Price : element.lp,
-                        PL : Math.round(qty*(shp-element.lp) - taxFactor*2*inv),
-                        Timing : timeLocal,
+                        PL : Math.round(qty*(shp-sl)-taxFactor*2*inv),
+                        Timing : timeSLHitLocal,
                         Qty :qty
-                    })  ;  
+                    })  ;         
                    // break;
+                }
+                
+                else if(element.lp<=target){
+                    const timeTargetHitLocal = new Date(element.ts).toLocaleTimeString();
+                   
+                    return({
+                        Symbol :i.Symbol,
+                        Type :'Target Hit',
+                        SP : shp,
+                        Price : element.lp,
+                        PL : Math.round(qty*(shp-target)-taxFactor*2*inv),
+                        Timing : timeTargetHitLocal,
+                        Qty :qty
+                    })  ;    
+                   // break;
+                }
+                
+                else{
+                    if(index===fid.length-1){
+                        const timeLocal = new Date(element.ts).toLocaleTimeString();
+    
+                        return({
+                            Symbol :i.Symbol,
+                            Type :'Closing',
+                            SP : shp,
+                            Price : element.lp,
+                            PL : Math.round(qty*(shp-element.lp) - taxFactor*2*inv),
+                            Timing : timeLocal,
+                            Qty :qty
+                        })  ;  
+                       // break;
+                    }
                 }
             }
         }
+    }catch(err){
+        console.log(`Error occured for SID ${i.Symbol} and error is ${err}`)
     }
+    
 }
 
 const evaluateShorts=async ()=>{
-   const sidShorts = data.filter(i=>shortSymbols.includes(i.Symbol));
-   // loop for sidShorts
-   const PlProms=sidShorts.map(x=>getPLForSymbol(x));
-   const PlRes = await Promise.all(PlProms);
-   // Build BO table
-   const BOTable = PlRes.map(i=>{
-        return {
-            Symbol : i.Symbol,
-            SL : (i.SP*slfactor).toFixed(1),
-            Target : (i.SP*targetfactor).toFixed(1),
-            Price : i.SP,
-            Qty : i.Qty
-        }
-   });
-   console.table(BOTable);
-   console.table(PlRes);
-   const tpl = PlRes.reduce((sum,o)=>sum+o.PL,0);
-   console.log(`Total PL is : ${tpl}`);
+    try{
+        const sidShorts = data.filter(i=>shortSymbols.includes(i.Symbol));
+      //  console.log(sidShorts);
+        // loop for sidShorts
+        const PlProms=sidShorts.map(x=>getPLForSymbol(x));
+        const PlRes = await Promise.all(PlProms);
+        // Build BO table
+        const BOTable = PlRes.map(i=>{
+             return {
+                 Symbol : i.Symbol,
+                 SL : (i.SP*slfactor).toFixed(1),
+                 Target : (i.SP*targetfactor).toFixed(1),
+                 Price : i.SP,
+                 Qty : i.Qty
+             }
+        });
+        console.table(BOTable);
+        console.table(PlRes);
+        const tpl = PlRes.reduce((sum,o)=>sum+o.PL,0);
+        console.log(`Total PL is : ${tpl}`);
+    }catch(err){
+        console.log(err);
+    }
+  
 }
 
 
